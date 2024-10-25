1f54329cfb6680ddb01dce1b4380b3deb3f5027e
dean <bernarddean.cuyco@intellisolution.tech>
2024-10-25 19:15:56 +0800

    fix lint

--- pages/url-search-engine/hooks/use-search-engine-payload.ts
+++ pages/url-search-engine/hooks/use-search-engine-payload.ts
@@ -7,7 +7,21 @@ export default function useSearchEnginePayload() {
 
     const [payload, setPayload] = useState<any>({})
 
-    const changePayload = createPayloadChanger(setPayload)
+    const changePayload = (accessor: string, input: any) => {
+        setPayload((current: any) => {
+            const keys = accessor?.split('.');
+            
+            const updateNestedObject = (obj: any, keys: string[], value: any): any => {
+                const key = keys[0];
+                if (keys.length === 1) {
+                    return { ...obj, [key]: value };
+                }
+                return { ...obj, [key]: updateNestedObject(obj[key] || {}, keys.slice(1), value) };
+            };
+            
+            return updateNestedObject(current, keys, input);
+        });
+    };
 
     return {
         payload, setPayload, changePayload,
