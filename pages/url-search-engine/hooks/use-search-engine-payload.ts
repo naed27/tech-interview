5f22b387ae39dfdac788959e6bdf9e3a6f4889a8
dean <bernarddean.cuyco@intellisolution.tech>
2024-11-04 12:25:39 +0800

    fix ts

--- pages/url-search-engine/hooks/use-search-engine-payload.ts
+++ pages/url-search-engine/hooks/use-search-engine-payload.ts
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import { createPayloadChanger } from "@/utils/form-helpers"
+import { createPayloadChanger } from "@/utils/form-helpers";
 
-export default function useSearchEnginePayload() {
+export default function useSearchEnginePayload(): {
+    payload: any;
+    setPayload: React.Dispatch<React.SetStateAction<any>>;
+    changePayload: (key: string, value: any) => void;
+} {
     const [payload, setPayload] = useState<any>({});
-
     const changePayload = createPayloadChanger(setPayload);
 
     return {
